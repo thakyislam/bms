121d32c54329187c08dd9c28e3ad24c9aaaca691
thakyislam <sadiataki11@gmail.com>
2024-06-04 16:55:56 +0200

    deceond

--- templates/edit_book_form.php
+++ templates/edit_book_form.php
@@ -1,10 +1,19 @@
 <form action="edit_book.php" method="post">
-    <input type="hidden" class="table-primary" name="id" value="<?php echo $book->id; ?>">
-    <label class="table-secondary">Title</label><br>
-    <input type="text" name="title" value="<?php echo $book->title; ?>"><br>
-    <label>Author</label><br>
-    <input type="text" name="author" value="<?php echo $book->author; ?>"><br>
-    <label>Published Year</label><br>
-    <input type="number" name="published_year" value="<?php echo $book->published_year; ?>"><br>
-    <input type="submit" value="Update Book">
+    <div class="text-start">
+        <div class="mb-3">
+            <input type="hidden" class="table-primary" name="id" value="<?= $book->id; ?>">
+            <label for="titleInput" class="form-label">Title</label><br>
+            <input type="text" class="form-control" name="title" value="<?= $book->title; ?>"><br>
+        </div>
+        <div class="mb-3">
+            <label for="authorInput" class="form-label">Author</label><br>
+            <input type="text" class="form-control" name="author" value="<?= $book->author; ?>"><br>
+        </div>
+        <div class="mb-3">
+            <label for="yearInput" class="form-label">Published Year</label><br>
+            <input type="number" class="form-control" name="published_year" value="<?= $book->published_year; ?>"><br>
+        </div>
+        <input type="submit" value="Update Book">
+    </div>
+    </div>
 </form>
